ca6ad0f2d4acf93e212b3e521fb09d283172af56
anvo1205 <39280916+anvo1205@users.noreply.github.com>
2022-11-20 15:07:38 -0500

    First commit: create framework structure

---
+++ testCases/com/NopCommerce/User/FirstClass.java
@@ -0,0 +1,24 @@
+package com.NopCommerce.User;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class FirstClass {
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@Test
+	public void TC_01() {
+	}
+	
+	@Test
+	public void TC_02() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+}
