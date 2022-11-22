de526eb502ec5d1d1aae6ade8d0369b197cb6cf8
anvo1205 <39280916+anvo1205@users.noreply.github.com>
2022-11-21 21:54:58 -0500

    Add some page objects and refactor tests using page objects

---
+++ actions/pageObjects/RegisterPageObject.java
@@ -0,0 +1,92 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.RegisterPageUI;
+
+public class RegisterPageObject extends BasePage{
+	WebDriver driver;
+	
+	public RegisterPageObject(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void clickRegisterButton() {
+		waitForElementClikable(driver, RegisterPageUI.REGISTER_BUTTON);
+		clickElement(driver, RegisterPageUI.REGISTER_BUTTON);
+		
+	}
+
+	public String getFirstNameErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_ERROR_MSG);
+		return getText(driver, RegisterPageUI.FIRST_NAME_ERROR_MSG);
+	}
+
+	public String getLastNameErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_ERROR_MSG);
+		return getText(driver, RegisterPageUI.LAST_NAME_ERROR_MSG);
+	}
+
+	public String getEmailErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_ERROR_MSG);
+		return getText(driver, RegisterPageUI.EMAIL_ERROR_MSG);
+	}
+
+	public String getPasswordErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_ERROR_MSG);
+		return getText(driver, RegisterPageUI.PASSWORD_ERROR_MSG);
+	}
+
+	public String getConfirmPasswordErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_ERROR_MSG);
+		return getText(driver, RegisterPageUI.CONFIRM_PASSWORD_ERROR_MSG);
+	}
+
+	public void inputFirstName(String firstName) {
+		waitForElementVisible(driver, RegisterPageUI.FIRST_NAME_TEXTBOX);
+		sendKeysToElement(driver, RegisterPageUI.FIRST_NAME_TEXTBOX, firstName);
+		
+	}
+
+	public void inputLastName(String lastName) {
+		waitForElementVisible(driver, RegisterPageUI.LAST_NAME_TEXTBOX);
+		sendKeysToElement(driver, RegisterPageUI.LAST_NAME_TEXTBOX, lastName);
+		
+	}
+
+	public void inputEmail(String email) {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_TEXTBOX);
+		sendKeysToElement(driver, RegisterPageUI.EMAIL_TEXTBOX, email);
+		
+	}
+
+	public void inputPassword(String password) {
+		waitForElementVisible(driver, RegisterPageUI.PASSWORD_TEXTBOX);
+		sendKeysToElement(driver, RegisterPageUI.PASSWORD_TEXTBOX, password);
+		
+	}
+
+	public void inputConfirmPassword(String confirmPassword) {
+		waitForElementVisible(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX);
+		sendKeysToElement(driver, RegisterPageUI.CONFIRM_PASSWORD_TEXTBOX, confirmPassword);
+		
+	}
+
+	public String getRegisterSuccessMsg() {
+		waitForElementVisible(driver, RegisterPageUI.REGISTER_SUCCESS_MSG);
+		return getText(driver, RegisterPageUI.REGISTER_SUCCESS_MSG);
+	}
+
+	public void clickLogOutLink() {
+		waitForElementClikable(driver, RegisterPageUI.LOG_OUT_LINK);
+		clickElement(driver, RegisterPageUI.LOG_OUT_LINK);
+		
+	}
+
+	public String getEmailAlreadyExistErrorMsg() {
+		waitForElementVisible(driver, RegisterPageUI.EMAIL_ALREADY_EXIST_ERROR_MSG);
+		return getText(driver, RegisterPageUI.EMAIL_ALREADY_EXIST_ERROR_MSG);
+	}
+
+}
---
+++ interfaces/pageUIs/HomePageUI.java
@@ -0,0 +1,8 @@
+package pageUIs;
+
+public class HomePageUI {
+	
+	public static final String REGISTER_LINK = "//a[@class='ico-register']";
+	
+
+}
